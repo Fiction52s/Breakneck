d46debaed92fc1fffe23157fb0ebd3163faeb1dc
Shephard Lima <fiction52s@gmail.com>
2015-04-28 18:32:00 -0700

    Revert "buggy"
    This reverts commit ddba3cf08dfa2ef48f71939252ab07516ca4e78b.

--- Breakneck/main.cpp
+++ Breakneck/main.cpp
@@ -36,8 +36,6 @@ struct Edge
 	Vector2f v1;
 	Edge * GetEdge0();
 	Edge * GetEdge1();
-	Edge *edge0;
-	Edge *edge1;
 	//material ---
 
 	Vector2f Normal()
@@ -92,13 +90,7 @@ struct CollisionBox
 
 struct Contact
 {
-	Contact()
-		:edge( NULL )
-	{
-	}
-	Vector2f position;
-	Vector2f resolution;
-	Edge *edge;
+
 };
 
 
@@ -275,18 +267,17 @@ sf::Vector2f lineIntersection( Vector2f a, Vector2f b, Vector2f c, Vector2f d )
 	return sf::Vector2f( x, y );
 }
 
-Contact *currentContact;
-Contact *collideEdge( Actor &a, const CollisionBox &b, Edge *e )
+void collideEdge( Actor &a, const CollisionBox &b, Edge &e )
 {
 	float left = a.position.x + b.offset.x - b.rw;
 	float right = a.position.x + b.offset.x + b.rw;
 	float top = a.position.y + b.offset.y - b.rh;
 	float bottom = a.position.y + b.offset.y + b.rh;
 
-	float edgeLeft = min( e->v0.x, e->v1.x );
-	float edgeRight = max( e->v0.x, e->v1.x ); 
-	float edgeTop = min( e->v0.y, e->v1.y ); 
-	float edgeBottom = max( e->v0.y, e->v1.y ); 
+	float edgeLeft = min( e.v0.x, e.v1.x );
+	float edgeRight = max( e.v0.x, e.v1.x ); 
+	float edgeTop = min( e.v0.y, e.v1.y ); 
+	float edgeBottom = max( e.v0.y, e.v1.y ); 
 
 	/*sf::RectangleShape r1( Vector2f(100,300) );
 	r1.setPosition( actorPos.x - 50, actorPos.y - 150 );
@@ -312,37 +303,25 @@ Contact *collideEdge( Actor &a, const CollisionBox &b, Edge *e )
 	if( aabbCollision )
 	{
 		Vector2f corner(0,0);
-		Vector2f edgeNormal = e->Normal();
+		Vector2f edgeNormal = e.Normal();
 		cout << "normal: " << edgeNormal.x << ", " << edgeNormal.y << endl;
-		bool resolve = false;
-
 		if( edgeNormal.x > 0 )
 		{
 			if( edgeNormal.y > 0 )
 			{
 				Vector2f topLeft( left, top );
 				corner = topLeft;
-				resolve = true;
 			}
 			else if( edgeNormal.y < 0 )
 			{
 				
 				Vector2f bottomLeft( left, bottom );
 				corner = bottomLeft;
-				resolve = true;
 			}
 			else
 			{
-				//Vector2f topLeft( top, left );
-				//Vector2f bottomLeft( bottom, left );
-
-			//	corner = Vector2f( left, (top + bottom) / 2 );
-			//	resolve = true;
-				//currentContact->position = (topLeft + bottomLeft) / 2;
-				//currentContact->resolution = intersect - currentContact->position;
-				//currentContact->edge = e;
-				
-
+				Vector2f topLeft( top, left );
+				Vector2f bottomLeft( bottom, left );
 			}
 		}
 		else if( edgeNormal.x < 0 )
@@ -351,21 +330,14 @@ Contact *collideEdge( Actor &a, const CollisionBox &b, Edge *e )
 			{
 				Vector2f topRight( right, top );
 				corner = topRight;
-				resolve = true;
 			}
 			else if( edgeNormal.y < 0 )
 			{
 				Vector2f bottomRight( right, bottom );
 				corner = bottomRight;
-				resolve = true;
 			}
 			else
 			{
-				//Vector2f topRight( right, top );
-				//Vector2f bottomRight( right, bottom );
-				//corner = (topRight + bottomRight) / 2;
-			//	corner = Vector2f( right, (top + bottom) / 2);
-			//	resolve = true;
 			}
 		}
 		else
@@ -373,18 +345,10 @@ Contact *collideEdge( Actor &a, const CollisionBox &b, Edge *e )
 			//up and down
 			if( edgeNormal.y > 0 )
 			{
-				//Vector2f bottomLeft( left, bottom );
-				//Vector2f bottomRight( right, bottom );
-			//	corner = Vector2f( (left + right) / 2, bottom );
-			//	resolve = true;
+
 			}
 			else if( edgeNormal.y < 0 )
 			{
-				//Vector2f topLeft( left, top );
-				//Vector2f topRight( right, top);
-				//corner = (topLeft + topRight) / 2;
-			//	corner = Vector2f( (left + right) / 2, top );
-			//	resolve = true;
 			}
 			else
 			{
@@ -392,16 +356,11 @@ Contact *collideEdge( Actor &a, const CollisionBox &b, Edge *e )
 			}
 		}
 
-		if( resolve )
-		{
-		//	Vector2f invVel = normalize(-a.velocity);
-		}
-
-		int res = cross( corner - e->v0, e->v1 - e->v0 );
-		if( res < 0 && resolve )
+		int res = cross( corner - e.v0, e.v1 - e.v0 );
+		if( res < 0 )
 		{
 			Vector2f invVel = normalize(-a.velocity);
-			Vector2f intersect = lineIntersection( corner, corner + invVel, e->v0, e->v1 );
+			Vector2f intersect = lineIntersection( corner, corner + invVel, e.v0, e.v1 );
 
 			sf::CircleShape cs;
 			cs.setFillColor( Color::Magenta );
@@ -414,19 +373,12 @@ Contact *collideEdge( Actor &a, const CollisionBox &b, Edge *e )
 			//cs.setPosition( bottomLeft - invVel );
 			//window->draw( cs );
 
-			//Vector2f p = intersect - corner;
-			currentContact->position = corner;
-			currentContact->resolution = intersect - corner;
-			currentContact->edge = e;
-
-			return currentContact;
-			//a.position = a.position + p;
+			Vector2f p = intersect - corner;
+			a.position = a.position + p;
 		}
 
 
 	}
-
-	return NULL;
 }
 
 void collideRectRect()
@@ -443,7 +395,7 @@ void collideRectRect()
 int main()
 {
 	window = new sf::RenderWindow(/*sf::VideoMode(1400, 900)sf::VideoMode::getDesktopMode()*/
-		sf::VideoMode( 1920 / 2, 1080 / 2), "Breakneck", sf::Style::None, sf::ContextSettings( 0, 0, 0, 0, 0 ));
+		sf::VideoMode( 1920 / 1, 1080 / 1), "Breakneck", sf::Style::None, sf::ContextSettings( 0, 0, 0, 0, 0 ));
 	sf::Vector2i pos( 0, 0 );
 
 	window->setPosition( pos );
@@ -455,132 +407,47 @@ int main()
 	window->setView( view );
 
 
-	currentContact = new Contact;
+	
 
 	
 	ifstream is;
 	is.open( "test.brknk" );
 
-	int numPoints;
-	is >> numPoints;
-	Vector2f *points = new Vector2f[numPoints];
 	list<Vector2f> lvec;
-	//load points
-	for( int i = 0; i < numPoints; ++i )
-	{
-		float px, py;
-		is >> px;
-		is >> py;
-			
-		points[i].x = px;
-		points[i].y = py;
-	}
-	
-	int totalEdges = numPoints;
-	int numPolygons;
-
-	is >> numPolygons;
-
-	
-	
-	Edge **edges = new Edge*[totalEdges];
-	
-	int currentEdgeIndex = 0;
-
-	//start loop
-
-	for( int n = 0; n < numPolygons; ++n )
-	{
-		string materialStr;
-		is >> materialStr;
-		int numPolygonEdges;
-		is >> numPolygonEdges;
-
-		for( int i = 0; i < numPolygonEdges; ++i )
-		{
-			Edge *ee = new Edge();
-			edges[currentEdgeIndex + i] = ee;
-			ee->v0 = points[i+currentEdgeIndex];
-			if( i < numPolygonEdges - 1 )
-				ee->v1 = points[i+1 + currentEdgeIndex];
-
-		
-
-			if( i == 0 )
-			{
-				ee->edge0 = edges[currentEdgeIndex + numPolygonEdges - 1];
-				ee->edge1 = edges[currentEdgeIndex + 1];
-			}
-			else if( i == numPolygonEdges - 1 )
-			{
-				ee->edge0 = edges[currentEdgeIndex + i - 1];
-				ee->edge1 = edges[currentEdgeIndex];
-				ee->v1 = points[currentEdgeIndex];
-			}
-			else
-			{
-				ee->edge0 = edges[currentEdgeIndex + i - 1];
-				ee->edge1 = edges[currentEdgeIndex + i + 1];
-			}
-
-			//cout << "edge " << i << " from: " << ee->v0.x << ", " << ee->v0.y << " to " << ee->v1.x << ", " << ee->v1.y << endl;
-		
-		}
-		currentEdgeIndex += numPolygonEdges;
-	}
-	//end loop
-
-	/*for( int i = 0; i < numEdges; ++i )
-	{
-		int v0i,v1i;
-		is >> v0i;
-		is >> v1i;
-
-		edges[i].v0 = points[v0i];
-		edges[i].v1 = points[v1i];
-	}*/
-
-	//int lineCount = lvec.size();
-	sf::Vertex *line = new sf::Vertex[totalEdges*2];
-	for( int i = 0; i < totalEdges; ++i )
-	{
-		//cout << "i: " << i << endl;
-		line[i*2] = sf::Vertex( edges[i]->v0 );
-		line[i*2+1] =  sf::Vertex( edges[i]->v1 );
-	}
-
-
-	//load edges
-
-	/*list<Edge*> eList;
-	
-
 	while( true )
 	{
 		string s;
 		is >> s;
-		if( s == "x" )
+		if( s == "e" )
 		{
 			break;
-
 		}
 		else
 		{
-			int i;
-			i = atoi( s.c_str() );
-			int i1;
-			string s1;
-			is >> s1;
-			i1 = atoi( s1.c_str() );
-			Edge *e = new Edge;
-			e->v0 = line
+			float f;
+			f = atof( s.c_str() );
+			float f1;
+			string s2;
+			is >> s2;
+			f1 = atof( s2.c_str() );
 
+			lvec.push_back( Vector2f( f, f1 ) );
 		}
-	}*/
-	
+	}
 	
 
-	
+	int lineCount = lvec.size();
+	sf::Vertex *line = new sf::Vertex[lineCount*2];
+	list<Vector2f>::iterator it = lvec.begin();
+	for( int i = 0; i < lineCount * 2 - 2; ++i )
+	{
+		line[i] = sf::Vertex( (*it) );
+		++it;
+		line[++i] =  sf::Vertex( (*it) );
+	}
+	line[lineCount*2 -1] = sf::Vertex( (*it) );
+	it = lvec.begin();
+	line[lineCount * 2 - 2] = sf::Vertex( (*it) );
 
 	sf::Vector2f nLine = line[1].position - line[0].position;
 	nLine = normalize( nLine );
@@ -590,15 +457,15 @@ int main()
 	sf::CircleShape circle( 30 );
 	circle.setFillColor( Color::Blue );
 
-	/*Edge * edgeList = new Edge[lineCount];
+	Edge * edgeList = new Edge[lineCount];
 	for( int i = 0; i < lineCount-1; ++i )
 	{
 		edgeList[i].v0 = line[i*2].position;
 		edgeList[i].v1 = line[i*2+1].position;
 	}
 	edgeList[lineCount-1].v0 = line[(lineCount-2)*2+1].position;
-	edgeList[lineCount-1].v1 = line[0].position;*/
-	//cout << "blah v1: " << edgeList[lineCount-1].v1.x << ", " << edgeList[lineCount-1].v1.y << endl;
+	edgeList[lineCount-1].v1 = line[0].position;
+	cout << "blah v1: " << edgeList[lineCount-1].v1.x << ", " << edgeList[lineCount-1].v1.y << endl;
 	/*Edge edge1;
 	edge1.v0 = sf::Vector2f(0, 600);
 	edge1.v1 = sf::Vector2f(1000, 1000);
@@ -627,7 +494,7 @@ int main()
 	b.type = b.Physics;
 
 	Actor player;
-	player.position = Vector2f( -500, 300 );
+	player.position = Vector2f( 300, 300 );
 
 	sf::Clock gameClock;
 	double currentTime = 0;
@@ -676,35 +543,26 @@ int main()
 
 			//Vector2f rCenter( r.getPosition().x + r.getLocalBounds().width / 2, r.getPosition().y + r.getLocalBounds().height / 2 );
 		
-			for( int i = 0; i < totalEdges; ++i )
+			for( int i = 0; i < lineCount; ++i )
 			{
-				Contact *c = collideEdge( player, b, edges[i] );
-				if( c != NULL )
-				{
-				player.position += c->resolution;
-				cout << "resolution: " << c->resolution.x << ", " << c->resolution.y << endl;
-				//break;
-				}
+				collideEdge( player, b, edgeList[i] );
 			}
 
 			player.UpdatePostPhysics();
 
 			//r.setPosition( player.position.x - r.getLocalBounds().width / 2, player.position.y - r.getLocalBounds().height / 2 );
-			cout << "playerPos: " << player.position.x << ", " << player.position.y << endl;	
+			
 		
 
 			accumulator -= TIMESTEP;
-			while( sf::Keyboard::isKeyPressed( sf::Keyboard::Space ) )
-		{
-		}
 		}
 
-		view.setSize( Vector2f( 960 * 1, 540 * 1 ) );
+		view.setSize( Vector2f( 960 * .5, 540 * .5 ) );
 		view.setCenter( player.position );
 		window->setView( view );
 		window->draw( *(player.sprite) );
 		window->draw( circle );
-		window->draw(line, totalEdges * 2, sf::Lines);
+		window->draw(line, lineCount * 2, sf::Lines);
 
 		/*Event ev;
 		window->pollEvent( ev );
@@ -729,8 +587,6 @@ int main()
 
 		if( sf::Keyboard::isKeyPressed( sf::Keyboard::Escape ) )
 				return 0;
-
-		
 
 
 		
